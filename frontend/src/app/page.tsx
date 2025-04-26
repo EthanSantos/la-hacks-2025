e1eb8e41c95ea7b27a77e2b0506f0dbf0e76fbbf
EthanSantos <ethanangelo61@gmail.com>
2025-04-26 10:48:29 -0700

    official api

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
    try {
      const response = await axios.post(
-       "http://127.0.0.1:5000/api/analyze",
+       "https://la-hacks-api.vercel.app/api/analyze",
        { 
          message,
          player_id: 1,
